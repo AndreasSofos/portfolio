076f6600f17c1f50c81be493d2a05cc587d17327
Andreas Sofos <andreassofos@andreass-macbook-pro.home>
2024-06-04 14:53:27 +0300

    Remove commented line

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  // return <Component {...pageProps} />;
   return (
     <>
       <Analytics />
